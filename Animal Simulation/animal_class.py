c990d88196dea56ec838809dfdad26a6ef2abdca
DanielOjo <danielojo@live.com>
2015-06-12 09:55:35 +0100

    animal work in progress

--- Animal Simulation/animal_class.py
+++ Animal Simulation/animal_class.py
@@ -1,33 +1,34 @@
 #Daniel Ogunlana
-#08/06/2015
-#Task 7(start)
+#12/06/2015
+#Task Animal
 
 import random
 
-class Crop:
+class Animal:
     """A geneic food crop"""
     #constructor
     def __init__(self,growth_rate, light_need,water_need):
         #set the attributes with am inital value
-        self._growth = 0
+        self._weight = 0
         self._days_growing = 0
         self._growth_rate = growth_rate
-        self._light_need = light_need
+        self._food_need = light_need
         self._water_need = water_need
-        self._status = "Seed"
+        self._status = "Baby"
         self._type = "Generic"
-
+        self._growth = 0
+        self._name = "Chris"
         #the above attributes are prefixed with an underscore to indicate
         #that they should not be accessed directly from outwith the class
 
     def needs(self):
         #return a dictionary containing the light and water needs
-        return{"light need":self._light_need,"water need":self._water_need}
+        return{"food need":self._food_need,"water need":self._water_need}
 
     #method to report the provide information about the current state
-    # of the crop
+    #of the animal
     def report(self):
-        return{"type":self._type, "status":self._status, "growth":self._growth, "days growing":self._days_growing}
+        return{"type":self._type, "status":self._status, "growth":self._growth, "days growing":self._days_growing, "name":self._name}
 
     def _update_status(self):
         if self._growth > 15:
@@ -37,32 +38,32 @@ class Crop:
         elif self._growth > 5:
             self._status = "Young"
         elif self._growth > 0:
-            self._status = "Seedling"
+            self._status = "Child"
         elif self._growth == 0:
-            self._status = "Seed"
+            self._status = "Baby"
 
-    def grow(self,light,water):
-        if light >= self._light_need and water >= self._water_need:
+    def grow(self,food,water):
+        if food >= self._food_need and water >= self._water_need:
             self._growth += self._growth_rate
         #increment days growing
         self._days_growing += 1
         #update the status
         self._update_status()
 
-def auto_grow(crop,days):
-    #grow the crop
+def auto_age(animal,days):
+    #age the animal
     for day in range(days):
-        light = random.randint(1,10)
+        food = random.randint(1,10)
         water = random.randint(1,10)
-        crop.grow(light,water)
+        animal.grow(food,water)
 
-def manual_grow(crop):
+def manual_age(animal):
     #get the light and water values from the user
     valid = False
     while not valid:
         try:
-            light = int(input("Please enter a light value between 1-10: "))
-            if 1 <= light <= 10:
+            food = int(input("Please enter a food value between 1-10: "))
+            if 1 <= food <= 10:
                 valid = True
             else:
                 print("Please enter a valid number between 1-10")
@@ -79,12 +80,12 @@ def manual_grow(crop):
         except ValueError:
             print("Please enter a valid water value between 1-10")
     #gorw the crop
-    crop.grow(light,water)
+    animal.grow(food,water)
     print()
         
 def display_menu():
-    print("1. Grow manually over 1 day")
-    print("2. Grow automatically over 30 days")
+    print("1. Age manually over 1 day")
+    print("2. Age automatically over 30 days")
     print("3. Report status")
     print("0. Exit program")
     print()
@@ -102,10 +103,10 @@ def get_menu_choice():
        except ValueError:
             print("Please enter a valid option")
     print()
-       return choice
+    return choice
 
-def manage_crop(crop):
-    print("This is the crop management program")
+def manage_animal(animal):
+    print("This is the animal management program")
     print()
     noexit = True
     while noexit:
@@ -113,21 +114,21 @@ def manage_crop(crop):
         option = get_menu_choice()
         print()
         if option == 1:
-            manual_grow(crop)
+            manual_age(animal)
         elif option == 2:
-            auto_grow(crop,30)
+            auto_age(animal,30)
         elif option == 3:
-            print(crop.report())
+            print(animal.report())
             print()
         elif option == 0:
             noexit = False
             print()
-    print("Thank you for using the crop management program")
+    print("Thank you for using the aniaml management program")
                                     
 def main():
     #instanciate the class
-    new_crop = Crop(1,4,3)
-    manage_crop(new_crop)
+    new_animal = Animal(1,4,3)
+    manage_animal(new_animal)
 
     
 
